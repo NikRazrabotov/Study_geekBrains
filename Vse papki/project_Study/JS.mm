50e69d08cb712034c250a776d971e225904f904c
Viktor Didov <razrabotov@yandex.ru>
2024-05-31 14:05:33 +0200

    Изучил_функции_Нужно_Записать_Функции_по_умолчанию._И_попрактиковаться_обязательно_иначе_не_запомню

--- Vse papki/project_Study/JS.mm
+++ Vse papki/project_Study/JS.mm
@@ -3,14 +3,14 @@
 <node TEXT="JS" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1716990932180" VGAP_QUANTITY="3 pt" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_FLOW">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <hook NAME="MapStyle" background="#3c3836" zoom="0.826">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" followedTemplateLocation="template:/dark_gruvbox_template.mm" followedMapLastTime="1714575758000" fit_to_viewport="false"/>
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" followedTemplateLocation="template:/dark_gruvbox_template.mm" followedMapLastTime="1699818412000" fit_to_viewport="false"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_602083445" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-18 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-17.25 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
 <richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
@@ -102,34 +102,84 @@
 </node>
 </node>
 </node>
-<node TEXT="if (){&#xa;&#xa;} else if () {&#xa;&#xa;} else {}" FOLDED="true" ID="ID_420451969" CREATED="1716980628057" MODIFIED="1716980705291">
-<node TEXT="const good = prompt(&quot;Введите товар&quot;);&#xa;if (good === &quot;Бананы&quot;) {&#xa;    console.log(&quot;Цена на бананы 50 руб./кг.&quot;);&#xa;} else if (good === &quot;Манго&quot;) {&#xa;    console.log(&quot;Цена на манго 80 руб./кг.&quot;);&#xa;} else if (good === &quot;Яблоки&quot; || good === &quot;Груши&quot;) {&#xa;    console.log(&quot;На яблоки и груши одна цена - 40 руб./кг.&quot;);&#xa;} else {&#xa;    console.log(&quot;Неизвестный фрукт&quot;);&#xa;}" FOLDED="true" ID="ID_1733249433" CREATED="1716981579078" MODIFIED="1716981584505">
-<node TEXT="Но когда много условий, читается плохо.&#xa;&#xa;для того что бы лучше читалось используй оператор switch" ID="ID_645478091" CREATED="1716989846754" MODIFIED="1716989917972">
-<node TEXT="switch (good) {&#xa;  case &quot;Бананы&quot;:&#xa;     console.log(&quot;Цена на бананы 50 руб./кг);&#xa;     break;&#xa;  case &quot;Манго&quot;:&#xa;     console.log(&quot;Цена на бананы 50 руб./кг);&#xa;     break;&#xa;  case &quot;Яблоки&quot;:&#xa;  case &quot;apples&quot;:&#xa;  case &quot;Яблочки&quot;:&#xa;  case &quot;Груши&quot;:&#xa;     console.log(&quot;Цена на бананы 50 руб./кг);&#xa;     break;&#xa;  default:" FOLDED="true" ID="ID_1681807878" CREATED="1716989919301" MODIFIED="1716990783998">
+<node TEXT="if (){&#xa;&#xa;} else if () {&#xa;&#xa;} else {}" FOLDED="true" ID="ID_420451969" CREATED="1716980628057" MODIFIED="1717146364263" ICON_SIZE="8 pt" COLOR="#96dbb0" BACKGROUND_COLOR="#035d99" STYLE="fork" SHAPE_HORIZONTAL_MARGIN="5 pt" SHAPE_VERTICAL_MARGIN="5 pt" UNIFORM_SHAPE="true" MAX_WIDTH="7 cm">
+<font SIZE="12"/>
+<node TEXT="if else" FOLDED="true" POSITION="bottom_or_right" ID="ID_1563083443" CREATED="1717146191846" MODIFIED="1717146201639">
+<node TEXT="const good = prompt(&quot;Введите товар&quot;);&#xa;if (good === &quot;Бананы&quot;) {&#xa;    console.log(&quot;Цена на бананы 50 руб./кг.&quot;);&#xa;} else if (good === &quot;Манго&quot;) {&#xa;    console.log(&quot;Цена на манго 80 руб./кг.&quot;);&#xa;} else if (good === &quot;Яблоки&quot; || good === &quot;Груши&quot;) {&#xa;    console.log(&quot;На яблоки и груши одна цена - 40 руб./кг.&quot;);&#xa;} else {&#xa;    console.log(&quot;Неизвестный фрукт&quot;);&#xa;}" POSITION="bottom_or_right" ID="ID_1733249433" CREATED="1716981579078" MODIFIED="1716981584505"/>
+</node>
+<node TEXT="switch создан для поиска чего-то" FOLDED="true" POSITION="bottom_or_right" ID="ID_1972499123" CREATED="1716990114210" MODIFIED="1716990125924">
+<node TEXT="switch (good) {&#xa;  case &quot;Бананы&quot;:&#xa;     console.log(&quot;Цена на бананы 50 руб./кг);&#xa;     break;&#xa;  case &quot;Манго&quot;:&#xa;     console.log(&quot;Цена на бананы 50 руб./кг);&#xa;     break;&#xa;  case &quot;Яблоки&quot;:&#xa;  case &quot;apples&quot;:&#xa;  case &quot;Яблочки&quot;:&#xa;  case &quot;Груши&quot;:&#xa;     console.log(&quot;Цена на бананы 50 руб./кг);&#xa;     break;&#xa;  default:" FOLDED="true" POSITION="bottom_or_right" ID="ID_1681807878" CREATED="1716989919301" MODIFIED="1716990783998">
 <node TEXT="На яблоках не встретил break, провалился ниже, и выплнил команду в Грушах. Своеобразная затычка вместо ||" ID="ID_677118162" CREATED="1716990647204" MODIFIED="1716990712932"/>
 <node TEXT="удобно искать вперечеслении" ID="ID_1645792214" CREATED="1716990786932" MODIFIED="1716990803819"/>
-</node>
-<node TEXT="switch создан для поиска чего-то" FOLDED="true" ID="ID_1972499123" CREATED="1716990114210" MODIFIED="1716990125924">
-<node TEXT="Что мы ищем?&#xa;Мы ищем что лежит в good" ID="ID_352625624" CREATED="1716990135761" MODIFIED="1716990152428">
+<node TEXT="Что мы ищем?&#xa;Мы ищем что лежит в good" POSITION="bottom_or_right" ID="ID_352625624" CREATED="1716990135761" MODIFIED="1716990152428">
 <node TEXT="Дальше предлагаются варианты, пишутся через case" ID="ID_536723500" CREATED="1716990164562" MODIFIED="1716990185259">
 <node TEXT="после двоеточия пишется что нужно сделать если вариант поиска совпал" ID="ID_679427718" CREATED="1716990192160" MODIFIED="1716990219314"/>
 </node>
 </node>
-<node TEXT="default:" ID="ID_72443531" CREATED="1716990240480" MODIFIED="1716990248443">
+<node TEXT="default:" POSITION="bottom_or_right" ID="ID_72443531" CREATED="1716990240480" MODIFIED="1716990248443">
 <node TEXT="Если мы ничего не нашили то выполняем defolt" ID="ID_1942942945" CREATED="1716990249160" MODIFIED="1716990268018"/>
 </node>
-<node TEXT="break; -- c анг. сломать" ID="ID_431440021" CREATED="1716990334959" MODIFIED="1716990358952">
+<node TEXT="break; -- c анг. сломать" POSITION="bottom_or_right" ID="ID_431440021" CREATED="1716990334959" MODIFIED="1716990358952">
 <node TEXT="Завершает код, что бы далее не искал" ID="ID_1027156989" CREATED="1716990359510" MODIFIED="1716990372913"/>
 </node>
 </node>
 </node>
 </node>
-</node>
-<node TEXT="пример" FOLDED="true" ID="ID_225427799" CREATED="1717062676618" MODIFIED="1717062679860">
+<node TEXT="пример" ID="ID_225427799" CREATED="1717062676618" MODIFIED="1717062679860">
 <node TEXT="const good = prompt(&quot;Введите товар&quot;);&#xa;&#xa;if (good === &quot;Бананы&quot;) {&#xa;    alert(&quot;Цена на бананы 50 руб./кг.&quot;);&#xa;} else if (good === &quot;Манго&quot;) {&#xa;    alert(&quot;Цена на манго 80 руб./кг.&quot;);&#xa;} else if (good === &quot;Яблоки&quot; || good === &quot;Груши&quot;) {&#xa;    alert(&quot;На яблоки и груши одна цена - 40 руб./кг.&quot;);&#xa;} else {&#xa;    alert(&quot;Неизвестный фрукт&quot;);&#xa;}" ID="ID_194103339" CREATED="1717062680161" MODIFIED="1717062683135"/>
 </node>
+<node TEXT="" ID="ID_1125551595" CREATED="1717146405606" MODIFIED="1717146405606"/>
 </node>
-<node TEXT="функция" POSITION="top_or_left" ID="ID_1667067628" CREATED="1716990923504" MODIFIED="1716990938938"/>
+<node TEXT="функция" POSITION="top_or_left" ID="ID_1667067628" CREATED="1716990923504" MODIFIED="1717147563800"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Что такое функция.
+    </p>
+    <p>
+      Функция - это отдельный кусочек кода, который нам возможно будет необходимо повторить несколько раз.
+    </p>
+    <p>
+      Что бы не копировать условие(действие) кода. Запихивается все это в функцию. (как в переменной может лежать куча данных, так и в функции)
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="function sayPrice() {&#xa;  console.log(&quot;Цена на бананы 50 руб./кг&quot;);&#xa;}" ID="ID_214859706" CREATED="1717153872261" MODIFIED="1717153955190">
+<font SIZE="10"/>
+<node TEXT="С начало создаем функцию, потом вызываем функцию.&#xa;&#xa;sayPrice();" ID="ID_1840223407" CREATED="1717153967179" MODIFIED="1717154002144"/>
+</node>
+<node TEXT="функции уже заложенны" POSITION="bottom_or_right" ID="ID_1858412795" CREATED="1717154009606" MODIFIED="1717154049827">
+<node TEXT="alert()" POSITION="bottom_or_right" ID="ID_1293715191" CREATED="1717154004838" MODIFIED="1717154009250"/>
+<node TEXT="prompt()" ID="ID_1012972083" CREATED="1717154050569" MODIFIED="1717154055675"/>
+</node>
+<node TEXT="что бы посмотреть что делает функция внутри.&#xa;в инспекторе кода нажать F11" ID="ID_675423633" CREATED="1717154116781" MODIFIED="1717154146018">
+<node TEXT="функцию можно объявлять где угодно" ID="ID_692893317" CREATED="1717154221524" MODIFIED="1717154236272"/>
+<node TEXT="функции можно передавать какие то данные" ID="ID_1378606838" CREATED="1717154300690" MODIFIED="1717154314781"/>
+</node>
+<node TEXT="Примеры" ID="ID_967434471" CREATED="1717154381647" MODIFIED="1717154385306">
+<node TEXT="let myGood = prompt(&quot;Введите товар&quot;);&#xa;sayPrice(myGood);&#xa;sayPrice(&quot;Манго&quot;);&#xa;&#xa;function sayPrice(good) {&#xa;  if (good === &quot;Бананы&quot;) {&#xa;    alert(&quot;Цена на бананы 50 руб./кг.&quot;);&#xa;  } else if (good === &quot;Манго&quot;) {&#xa;    alert(&quot;Цена на манго 80 руб./кг.&quot;);&#xa;  } else if (good === &quot;Яблоки&quot; || good ===    &quot;Груши&quot;) {&#xa;    alert(&quot;На яблоки и груши одна цена - 40 руб./кг.&quot;);&#xa;  } else {&#xa;    alert(&quot;Неизвестный фрукт&quot;);&#xa;  }&#xa;}" ID="ID_1756083800" CREATED="1717154393808" MODIFIED="1717154628997" MAX_WIDTH="20 cm">
+<font SIZE="12"/>
+<node TEXT="sayPrice(myGood);" ID="ID_108162713" CREATED="1717155625582" MODIFIED="1717155642793">
+<node TEXT="В скобках называется &quot;Передать аргумент в параметр функции&quot;" ID="ID_1520543413" CREATED="1717155643159" MODIFIED="1717155666764"/>
+</node>
+<node TEXT="sayPrice(myGood); Передали значение в функцию,&#xa;а она в ответ выдает цену" ID="ID_518600948" CREATED="1717154630266" MODIFIED="1717154674302"/>
+<node TEXT="function sayPrice(good, red, car)" ID="ID_312238026" CREATED="1717155488389" MODIFIED="1717155524793">
+<node TEXT="В скобках это параметры." ID="ID_160648864" CREATED="1717155501318" MODIFIED="1717155513480"/>
+<node TEXT="Можно принимать функцией множество параметров&#xa;good, red, car = это названия переменных расположеных внутри функции" ID="ID_1591095429" CREATED="1717155526519" MODIFIED="1717155599432"/>
+</node>
+<node TEXT="по умолчанию" ID="ID_1073299876" CREATED="1717155900273" MODIFIED="1717155904844">
+<node TEXT="умолчанию.png" ID="ID_1570593337" CREATED="1717155915512" MODIFIED="1717155915512">
+<hook URI="JS_files/умолчанию.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="Имеется ввиду что в последнем sayPrice() ни чего не передали. А если ничего не передали то выведет Undefiend, что бы этого не было, нужно прописать&#xa;function sayPrice(price = &quot;Неизвестна.&quot;) {" ID="ID_951190296" CREATED="1717155927819" MODIFIED="1717156015892"/>
+</node>
+</node>
+</node>
+</node>
+</node>
 <node TEXT="Циклы" POSITION="top_or_left" ID="ID_536273191" CREATED="1717052718252" MODIFIED="1717052721927">
 <node TEXT="while" FOLDED="true" ID="ID_596111608" CREATED="1717052722255" MODIFIED="1717052732294">
 <node TEXT="let counter1 = 0;&#xa;&#xa;while (counter1 &lt;5) {&#xa;console.log(&quot;*****------&quot;);&#xa;counter1 ++;&#xa;}" ID="ID_684547301" CREATED="1717052732722" MODIFIED="1717052828780">
