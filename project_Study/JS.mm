100f9a9fa871931eb61b589dc900f007e1364e02
NikRazrabotov <razrabotov@yandex.ru>
2024-05-31 06:56:20 +0200

    Первый_коммит

---
+++ project_Study/JS.mm
@@ -0,0 +1,149 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="JS" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1716990932180" VGAP_QUANTITY="3 pt" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_FLOW">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#3c3836" zoom="0.826">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" followedTemplateLocation="template:/dark_gruvbox_template.mm" followedMapLastTime="1714575758000" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_602083445" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-18 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#458588"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_749235638" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_749235638"/>
+<font SIZE="12" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#282828" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#282828" BACKGROUND_COLOR="#ebdbb2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#d79921" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#98971a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffffff" BACKGROUND_COLOR="#b16286" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffff" BACKGROUND_COLOR="#689d6a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffffff" BACKGROUND_COLOR="#a89984" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ebdbb2" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#ebdbb2">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="переменные объявления&#xa;добавление и т д" FOLDED="true" POSITION="bottom_or_right" ID="ID_602536385" CREATED="1717053944767" MODIFIED="1717063026018" COLOR="#83bcd5" BACKGROUND_COLOR="#5e065e">
+<font SIZE="12"/>
+<node TEXT="let p = 1;&#xa;p++;" ID="ID_75442767" CREATED="1717053966612" MODIFIED="1717053988777">
+<node TEXT="Добавляет 1" ID="ID_1376253913" CREATED="1717053998466" MODIFIED="1717054005042"/>
+</node>
+<node TEXT="let p = 1;&#xa;p += 32;" ID="ID_766762602" CREATED="1717053966612" MODIFIED="1717054020196">
+<node TEXT="Добавляет 32" ID="ID_1670219611" CREATED="1717054020817" MODIFIED="1717054029253"/>
+</node>
+</node>
+<node TEXT="Условия" FOLDED="true" POSITION="bottom_or_right" ID="ID_1165473792" CREATED="1716979630581" MODIFIED="1716979637263">
+<node TEXT="if else" FOLDED="true" ID="ID_960852950" CREATED="1716979638092" MODIFIED="1716980033793" VGAP_QUANTITY="3 pt">
+<node TEXT="const goodsCount = 0;&#xa;if (goodsCount &gt; 0) {&#xa;  console.log(`Количество товаров: ${goodsCount}`);&#xa;} else {&#xa;  console.log(`Корзина пуста.`);&#xa;}" ID="ID_1999804357" CREATED="1716979648838" MODIFIED="1716980490755" HGAP_QUANTITY="24.5 pt" MAX_WIDTH="20 cm" MIN_WIDTH="11 cm"/>
+<node TEXT="операнды" ID="ID_246474336" CREATED="1716980462699" MODIFIED="1716980618203">
+<node TEXT="унарные" ID="ID_118593488" CREATED="1716980469890" MODIFIED="1716980472868">
+<node TEXT="+(prompt)" ID="ID_286745359" CREATED="1716980522850" MODIFIED="1716980537572">
+<node TEXT="+" ID="ID_502505972" CREATED="1716980537890" MODIFIED="1716980542692"/>
+</node>
+</node>
+<node TEXT="бинарные" ID="ID_696381767" CREATED="1716980473155" MODIFIED="1716980475668">
+<node TEXT="const name = &quot;Viktor&quot;;" ID="ID_1607396888" CREATED="1716980551498" MODIFIED="1716980571860">
+<node TEXT="бинарным операндом будет =&#xa;соединяет два оператора" ID="ID_461871598" CREATED="1716980572225" MODIFIED="1716980611515"/>
+</node>
+</node>
+<node TEXT="тернарные" FOLDED="true" ID="ID_604392943" CREATED="1716980475946" MODIFIED="1716980478709">
+<node TEXT="goodsCount &gt; 0&#xa;  ? console.log(`Количество товаров: ${goodsCount}`)&#xa;  : console.log(`Корзина пуста.`);&#xa;&#xa;console.log(goodsCount &gt; 0 ? `Количество товаров: ${goodsCount}` : `Корзина пуста.`" ID="ID_1394391282" CREATED="1716980480139" MODIFIED="1716980494072"/>
+<node TEXT="? :" ID="ID_728105591" CREATED="1716980497410" MODIFIED="1716980510964"/>
+</node>
+</node>
+</node>
+<node TEXT="if (){&#xa;&#xa;} else if () {&#xa;&#xa;} else {}" FOLDED="true" ID="ID_420451969" CREATED="1716980628057" MODIFIED="1716980705291">
+<node TEXT="const good = prompt(&quot;Введите товар&quot;);&#xa;if (good === &quot;Бананы&quot;) {&#xa;    console.log(&quot;Цена на бананы 50 руб./кг.&quot;);&#xa;} else if (good === &quot;Манго&quot;) {&#xa;    console.log(&quot;Цена на манго 80 руб./кг.&quot;);&#xa;} else if (good === &quot;Яблоки&quot; || good === &quot;Груши&quot;) {&#xa;    console.log(&quot;На яблоки и груши одна цена - 40 руб./кг.&quot;);&#xa;} else {&#xa;    console.log(&quot;Неизвестный фрукт&quot;);&#xa;}" FOLDED="true" ID="ID_1733249433" CREATED="1716981579078" MODIFIED="1716981584505">
+<node TEXT="Но когда много условий, читается плохо.&#xa;&#xa;для того что бы лучше читалось используй оператор switch" ID="ID_645478091" CREATED="1716989846754" MODIFIED="1716989917972">
+<node TEXT="switch (good) {&#xa;  case &quot;Бананы&quot;:&#xa;     console.log(&quot;Цена на бананы 50 руб./кг);&#xa;     break;&#xa;  case &quot;Манго&quot;:&#xa;     console.log(&quot;Цена на бананы 50 руб./кг);&#xa;     break;&#xa;  case &quot;Яблоки&quot;:&#xa;  case &quot;apples&quot;:&#xa;  case &quot;Яблочки&quot;:&#xa;  case &quot;Груши&quot;:&#xa;     console.log(&quot;Цена на бананы 50 руб./кг);&#xa;     break;&#xa;  default:" FOLDED="true" ID="ID_1681807878" CREATED="1716989919301" MODIFIED="1716990783998">
+<node TEXT="На яблоках не встретил break, провалился ниже, и выплнил команду в Грушах. Своеобразная затычка вместо ||" ID="ID_677118162" CREATED="1716990647204" MODIFIED="1716990712932"/>
+<node TEXT="удобно искать вперечеслении" ID="ID_1645792214" CREATED="1716990786932" MODIFIED="1716990803819"/>
+</node>
+<node TEXT="switch создан для поиска чего-то" FOLDED="true" ID="ID_1972499123" CREATED="1716990114210" MODIFIED="1716990125924">
+<node TEXT="Что мы ищем?&#xa;Мы ищем что лежит в good" ID="ID_352625624" CREATED="1716990135761" MODIFIED="1716990152428">
+<node TEXT="Дальше предлагаются варианты, пишутся через case" ID="ID_536723500" CREATED="1716990164562" MODIFIED="1716990185259">
+<node TEXT="после двоеточия пишется что нужно сделать если вариант поиска совпал" ID="ID_679427718" CREATED="1716990192160" MODIFIED="1716990219314"/>
+</node>
+</node>
+<node TEXT="default:" ID="ID_72443531" CREATED="1716990240480" MODIFIED="1716990248443">
+<node TEXT="Если мы ничего не нашили то выполняем defolt" ID="ID_1942942945" CREATED="1716990249160" MODIFIED="1716990268018"/>
+</node>
+<node TEXT="break; -- c анг. сломать" ID="ID_431440021" CREATED="1716990334959" MODIFIED="1716990358952">
+<node TEXT="Завершает код, что бы далее не искал" ID="ID_1027156989" CREATED="1716990359510" MODIFIED="1716990372913"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="пример" FOLDED="true" ID="ID_225427799" CREATED="1717062676618" MODIFIED="1717062679860">
+<node TEXT="const good = prompt(&quot;Введите товар&quot;);&#xa;&#xa;if (good === &quot;Бананы&quot;) {&#xa;    alert(&quot;Цена на бананы 50 руб./кг.&quot;);&#xa;} else if (good === &quot;Манго&quot;) {&#xa;    alert(&quot;Цена на манго 80 руб./кг.&quot;);&#xa;} else if (good === &quot;Яблоки&quot; || good === &quot;Груши&quot;) {&#xa;    alert(&quot;На яблоки и груши одна цена - 40 руб./кг.&quot;);&#xa;} else {&#xa;    alert(&quot;Неизвестный фрукт&quot;);&#xa;}" ID="ID_194103339" CREATED="1717062680161" MODIFIED="1717062683135"/>
+</node>
+</node>
+<node TEXT="функция" POSITION="top_or_left" ID="ID_1667067628" CREATED="1716990923504" MODIFIED="1716990938938"/>
+<node TEXT="Циклы" POSITION="top_or_left" ID="ID_536273191" CREATED="1717052718252" MODIFIED="1717052721927">
+<node TEXT="while" FOLDED="true" ID="ID_596111608" CREATED="1717052722255" MODIFIED="1717052732294">
+<node TEXT="let counter1 = 0;&#xa;&#xa;while (counter1 &lt;5) {&#xa;console.log(&quot;*****------&quot;);&#xa;counter1 ++;&#xa;}" ID="ID_684547301" CREATED="1717052732722" MODIFIED="1717052828780">
+<node TEXT="цикл будет бесконечно крутиться пока не обновить переменую до false" ID="ID_1724129213" CREATED="1717052998473" MODIFIED="1717053040527"/>
+</node>
+</node>
+<node TEXT="for" ID="ID_78895640" CREATED="1717052847560" MODIFIED="1717052852719">
+<node TEXT="Проще и меньше писать" FOLDED="true" ID="ID_543192394" CREATED="1717052853235" MODIFIED="1717052861433">
+<node TEXT="for (let counter1 = 0; counter1 &lt; 5; counter1 ++;) {&#xa;console.log(&quot;*****--------&quot;);&#xa;}" ID="ID_231256347" CREATED="1717052871131" MODIFIED="1717052991011"/>
+</node>
+<node TEXT="интересный пример" FOLDED="true" ID="ID_211509912" CREATED="1717053293742" MODIFIED="1717053303237">
+<node TEXT="for (let i = 1; i &lt; 5; i++) {&#xa;console.log(&quot;Round &quot; + i);&#xa;}" ID="ID_1350993349" CREATED="1717053303471" MODIFIED="1717053370997"/>
+</node>
+</node>
+</node>
+</node>
+</map>
