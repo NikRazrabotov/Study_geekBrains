3a61119396e1b285cc2d319c766e8dcc68f749cd
Viktor Didov <razrabotov@yandex.ru>
2024-06-24 19:44:07 +0200

    Начал_составлять_счёт_мнемотехники

---
+++ Vse papki/plans/MindCards/Счёт - мнемотехника/Счёт - Мнемотехника.mm
@@ -0,0 +1,135 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Счёт - Мнемотехника" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1719247410970"><hook NAME="MapStyle" background="#2e3440">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/dark_nord_template.mm" followedTemplateLocation="template:/dark_nord_template.mm" followedMapLastTime="1699818412000"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="1-10" FOLDED="true" POSITION="bottom_or_right" ID="ID_475929825" CREATED="1719247411365" MODIFIED="1719247417255">
+<node TEXT="1 - Копейщица" ID="ID_1944924192" CREATED="1719247418347" MODIFIED="1719247435151"/>
+<node TEXT="2 - Лебедь" ID="ID_1035545913" CREATED="1719247435688" MODIFIED="1719247442266"/>
+<node TEXT="3 - Тор - Трезубец" ID="ID_866915911" CREATED="1719247442509" MODIFIED="1719247454347"/>
+<node TEXT="4 - Перевернутый стул" ID="ID_27244188" CREATED="1719247454629" MODIFIED="1719247464396"/>
+<node TEXT="5 - Капитан Крюк" ID="ID_1004390249" CREATED="1719247464678" MODIFIED="1719247473013"/>
+<node TEXT="6 - Замок" ID="ID_1685423351" CREATED="1719247473255" MODIFIED="1719247477560"/>
+<node TEXT="7 - Коса Смерти. Смерь" ID="ID_784781091" CREATED="1719247477817" MODIFIED="1719247490741"/>
+<node TEXT="8 - Очки" ID="ID_1524625251" CREATED="1719247490982" MODIFIED="1719247495677"/>
+<node TEXT="9 - Поварешка" ID="ID_340702784" CREATED="1719247495906" MODIFIED="1719247501822"/>
+<node TEXT="10 - Яблоко" ID="ID_743369146" CREATED="1719247502036" MODIFIED="1719247508883"/>
+</node>
+<node TEXT="11 - 20" FOLDED="true" POSITION="bottom_or_right" ID="ID_1644451903" CREATED="1719247512740" MODIFIED="1719247517069">
+<node TEXT="11 - Барабанные палочки" ID="ID_1483923735" CREATED="1719247517442" MODIFIED="1719247808516"/>
+<node TEXT="12 - Календарь" FOLDED="true" ID="ID_145903096" CREATED="1719247524694" MODIFIED="1719247724988">
+<node TEXT="12.png" ID="ID_1993279771" CREATED="1719247783439" MODIFIED="1719247783439">
+<hook URI="Счёт%20-%20Мнемотехника_files/12.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="13 - Сиськи" ID="ID_1030710182" CREATED="1719247809712" MODIFIED="1719247928918"/>
+<node TEXT="14 - Паспорт" ID="ID_1620966961" CREATED="1719247930005" MODIFIED="1719248060896"/>
+<node TEXT="15 - Ким 5+" ID="ID_1667312631" CREATED="1719248061316" MODIFIED="1719248162429">
+<node TEXT="Типа два слова по этому две цифры" FOLDED="true" ID="ID_926974200" CREATED="1719248162714" MODIFIED="1719248172353">
+<node TEXT="15.png" ID="ID_903557506" CREATED="1719248449744" MODIFIED="1719248449744">
+<hook URI="Счёт%20-%20Мнемотехника_files/15.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="16 - средний размер члена" ID="ID_446548367" CREATED="1719248638498" MODIFIED="1719249188621">
+<node TEXT="В общем член" ID="ID_620348241" CREATED="1719249188924" MODIFIED="1719249192816"/>
+</node>
+<node TEXT="17 - смерь с копьём и косой" ID="ID_1068583067" CREATED="1719249199129" MODIFIED="1719249231143"/>
+<node TEXT="18 - матрешка держит копьё в правой руке, от меня с лева" ID="ID_443977144" CREATED="1719249231572" MODIFIED="1719249267945"/>
+<node TEXT="19 - башенный кран с крюком" ID="ID_210765903" CREATED="1719249268377" MODIFIED="1719250623839"/>
+<node TEXT="20 - двойной световой меч" ID="ID_574662146" CREATED="1719249303090" MODIFIED="1719250748043"/>
+</node>
+<node TEXT="21 - 30" POSITION="bottom_or_right" ID="ID_1193928648" CREATED="1719250761643" MODIFIED="1719250772349">
+<node TEXT="21 - арбуз" ID="ID_39235886" CREATED="1719250772944" MODIFIED="1719250780725"/>
+<node TEXT="22 - Бананы" ID="ID_1024098374" CREATED="1719250781363" MODIFIED="1719250791751"/>
+<node TEXT="23 - Виноград" ID="ID_210235878" CREATED="1719250792386" MODIFIED="1719250814437"/>
+<node TEXT="24 - Гермиона" ID="ID_1503630491" CREATED="1719250814902" MODIFIED="1719250833874"/>
+<node TEXT="25 - Доширак" ID="ID_92934713" CREATED="1719250834724" MODIFIED="1719250873036"/>
+<node TEXT="26 - Енот" ID="ID_868923311" CREATED="1719250873346" MODIFIED="1719250908329"/>
+<node TEXT="27 - Ёлка" ID="ID_637855990" CREATED="1719250909249" MODIFIED="1719250924960"/>
+<node TEXT="28 - Жёлудь" ID="ID_564057290" CREATED="1719250925195" MODIFIED="1719250965873"/>
+<node TEXT="29 - Заяц" ID="ID_26502860" CREATED="1719250966133" MODIFIED="1719250978229"/>
+<node TEXT="30 - Икар" ID="ID_76387259" CREATED="1719250978744" MODIFIED="1719250995790"/>
+</node>
+<node TEXT="36" OBJECT="java.lang.Long|36" POSITION="top_or_left" ID="ID_183993305" CREATED="1719249446220" MODIFIED="1719249460252">
+<node TEXT="рак" FOLDED="true" ID="ID_337810239" CREATED="1719249460510" MODIFIED="1719249472187">
+<node TEXT="Слабо напоминает.&#xa;Если поделить на два то есть 3 одна сторона лап, 6 туловище и лапы" ID="ID_559673300" CREATED="1719249472468" MODIFIED="1719249523036"/>
+</node>
+</node>
+</node>
+</map>
