8f2297f2476f4a20ee0ecca78a03e981faa9b036
NikRazrabotov <razrabotov@yandex.ru>
2024-07-10 23:00:08 +0200

    Добавил_Карту_учителя

---
+++ Vse papki/plans/MindCards/Обучение с помощью Сенсея.mm
@@ -0,0 +1,135 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Обучение с помощью Сенсея" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1720338415613" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#3c3836">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_602083445" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-18.75 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#458588"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_749235638" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_749235638"/>
+<font SIZE="12" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#282828" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#282828" BACKGROUND_COLOR="#ebdbb2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#d79921" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#98971a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffffff" BACKGROUND_COLOR="#b16286" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffff" BACKGROUND_COLOR="#689d6a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffffff" BACKGROUND_COLOR="#a89984" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ebdbb2" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#ebdbb2">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Установить NodeJs" POSITION="bottom_or_right" ID="ID_635574381" CREATED="1720338416130" MODIFIED="1720339672197" HGAP_QUANTITY="20.75 pt" VSHIFT_QUANTITY="-33 pt"/>
+<node TEXT="Консоль" POSITION="bottom_or_right" ID="ID_367582217" CREATED="1720339658880" MODIFIED="1720339712379" HGAP_QUANTITY="35 pt" VSHIFT_QUANTITY="-2.25 pt">
+<node TEXT="Все что тебе надо знать про команды в проекте" ID="ID_1830504866" CREATED="1720339712649" MODIFIED="1720339734088">
+<node TEXT="npm init vite@latest &lt;project-name&gt; --template vue" ID="ID_837597270" CREATED="1720339726332" MODIFIED="1720339730316">
+<node TEXT="Это команда создает полностью проект  &lt;project-name&gt; - это имя проекта, назвать можешь как хочешь, лучше латиницей и без пробелов в имени" ID="ID_1884030227" CREATED="1720339735708" MODIFIED="1720339737227"/>
+</node>
+<node TEXT="cd &lt;project-name&gt;" ID="ID_1138799062" CREATED="1720339757146" MODIFIED="1720339762946">
+<node ID="ID_1614734459" CREATED="1720339764366" MODIFIED="1720339764366"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Это стандартная команда перехода в папку с названием проекта
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="https://v3.ru.vuejs.org/ru/guide/installation.html#vite" ID="ID_1129046370" CREATED="1720339810827" MODIFIED="1720342923084">
+<node TEXT="Смотри, вои инструкция по установке Vue https://v3.ru.vuejs.org/ru/guide/installation.html#cli" ID="ID_549978812" CREATED="1720339812748" MODIFIED="1720339812748" LINK="https://v3.ru.vuejs.org/ru/guide/installation.html#cli"/>
+<node TEXT="Не вчитывайся в нее." ID="ID_701369252" CREATED="1720339812779" MODIFIED="1720339812779"/>
+<node TEXT="Тебе надо от туда тольок # npm 6.x" FOLDED="true" ID="ID_1396093990" CREATED="1720339812795" MODIFIED="1720339812795">
+<node TEXT="Тебе надо от туда тольок # npm 6.x&#xa;$ npm init vite@latest &lt;project-name&gt; --template vue&#xa;&#xa;# npm 7+, НУЖНО дополнительное двойное тире:&#xa;$ npm init vite@latest &lt;project-name&gt; -- --template vue&#xa;&#xa;$ cd &lt;project-name&gt;&#xa;$ npm install&#xa;$ npm run dev" POSITION="bottom_or_right" ID="ID_674346692" CREATED="1720339844908" MODIFIED="1720339849937"/>
+<node TEXT="$ npm init vite@latest &lt;project-name&gt; --template vue" POSITION="bottom_or_right" ID="ID_43257576" CREATED="1720339812795" MODIFIED="1720339812795" LINK="mailto:vite@latest"/>
+<node TEXT="# npm 7+, НУЖНО дополнительное двойное тире:" POSITION="bottom_or_right" ID="ID_1157366058" CREATED="1720339812795" MODIFIED="1720339812795"/>
+<node TEXT="$ npm init vite@latest &lt;project-name&gt; -- --template vue" POSITION="bottom_or_right" ID="ID_987728959" CREATED="1720339812810" MODIFIED="1720339812810" LINK="mailto:vite@latest"/>
+<node TEXT="$ cd &lt;project-name&gt;" POSITION="bottom_or_right" ID="ID_1233645252" CREATED="1720339812810" MODIFIED="1720339812810"/>
+<node TEXT="$ npm install" POSITION="bottom_or_right" ID="ID_1132552397" CREATED="1720339812817" MODIFIED="1720339812817"/>
+<node TEXT="$ npm run dev" POSITION="bottom_or_right" ID="ID_1577485716" CREATED="1720339812817" MODIFIED="1720339812817"/>
+<node TEXT="Андрей Системщик Программист, [07.07.2024 10:02]" POSITION="bottom_or_right" ID="ID_194690137" CREATED="1720339812817" MODIFIED="1720339812817"/>
+<node TEXT="какая версия  npm  ты можешь узнать у себя через npm -v" POSITION="bottom_or_right" ID="ID_954955870" CREATED="1720339812817" MODIFIED="1720339812817"/>
+<node TEXT="npm run dev" POSITION="bottom_or_right" ID="ID_1445000768" CREATED="1720342680557" MODIFIED="1720342680557"/>
+</node>
+<node TEXT="структура развернутого npm" ID="ID_1968902803" CREATED="1720344014902" MODIFIED="1720344037900">
+<node TEXT="1.png" ID="ID_425304632" CREATED="1720344074236" MODIFIED="1720344074236">
+<hook URI="Обучение%20с%20помощью%20Сенсея_files/1.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="2.png" ID="ID_1233547408" CREATED="1720344093783" MODIFIED="1720344093783">
+<hook URI="Обучение%20с%20помощью%20Сенсея_files/2.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="Андрей Системщик Программист, [07.07.2024 11:19]&#xa;Отлично, удали папку components  она пока для тебя лищняя, и у тебя сразу все сломается.&#xa;&#xa;Андрей Системщик Программист, [07.07.2024 11:20]&#xa;Как разу увидишь уведомления об ошибках как их показывает Vue" ID="ID_1932605216" CREATED="1720344097175" MODIFIED="1720344116953"/>
+</node>
+<node TEXT="3.png" FOLDED="true" ID="ID_281992125" CREATED="1720344683236" MODIFIED="1720344683236">
+<hook URI="Обучение%20с%20помощью%20Сенсея_files/3.png" SIZE="0.8450704" NAME="ExternalObject"/>
+<node TEXT="Открыл App.vue" ID="ID_1288753265" CREATED="1720344690057" MODIFIED="1720344701464">
+<node TEXT="привел к шаблону как в картинке" ID="ID_645478299" CREATED="1720344702350" MODIFIED="1720344713498"/>
+</node>
+</node>
+</node>
+<node TEXT="Открывать лучше через vs code предварительно установив расширение" ID="ID_1682319181" CREATED="1720344740428" MODIFIED="1720344763431">
+<node TEXT="npm run dev" ID="ID_1537015664" CREATED="1720342680557" MODIFIED="1720342680557"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
